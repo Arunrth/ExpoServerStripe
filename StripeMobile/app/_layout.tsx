390763429d0b93ad3f56375b30b46b2ca74cb838
Arunrth <arunrthwm@gmail.com>
2025-01-04 12:42:13 +0530

    added stripe key to env file and hanled publish key

--- StripeMobile/app/_layout.tsx
+++ StripeMobile/app/_layout.tsx
@@ -1,10 +1,20 @@
 import { Stack } from "expo-router";
+import { StripeProvider } from '@stripe/stripe-react-native';
 
-const Layout =()=>{
-    return(
-        <Stack screenOptions={{headerShown:false}}>
-            <Stack.Screen name="index" />
-        </Stack>
+const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_API_KEY
+
+const Layout = () => {
+    if(!PUBLISHABLE_KEY) {
+        console.log('Stripe publishable key is missing. Check your .env file.')
+        return null
+    }
+    return (
+        <StripeProvider publishableKey={PUBLISHABLE_KEY}>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="index" />
+            </Stack>
+        </StripeProvider>
+
     )
 }
 
